b8b338b65d7ea327dc8d600f44b2117a35506cad
Djhonatas <dinho.gamerfps@gmail.com>
2021-11-10 11:37:43 -0300

    reposicao

--- readme.md
+++ readme.md
@@ -1,7 +1,10 @@
 # Github
 
-- Git init 
-- Git add . 
+- Git init// não dá git init, animal!!
+
+- Git add . // começa por essa porra.
+
+
 - Git commit -m "modificação feita"
 - git push origin master
 - Git remote add origin 'minha URL'
--- src/mocks/bordado.js
+++ src/mocks/bordado.js
@@ -2,14 +2,14 @@ import tela4 from '../../assets/tela4.jpeg';
 
 const bordado = {
     topo: {
-        titulo: "Detalhe do bordado"
+        titulo: "Funcional"
     },
     detalhes: {
-        nome:           "Águia Bordado",
+        nome:           "Detalhes do Bordado",
         imagemCliente:  tela4,
         nomeCliente:    "Val Castro",
         descricao:      "Bordado feito no ano de 2019",
-        preco:          "R$ 55,00",
+        preco:          "R$ 45,00",
         
     }
 }
